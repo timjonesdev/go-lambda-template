67fcabf46a6c3736a15baf3021cadf0867af2565
Ryan Monroe <rx364p@att.com>
2019-05-24 14:16:50 -0700

    Modified Dockerfile and implemented test

--- base/main_test.go
+++ base/main_test.go
@@ -1,1 +1,13 @@
 package main
+
+import "testing"
+
+func TestHandleRequest(t *testing.T) {
+	returnVal,a := HandleRequest(nil, MyEvent{"Kappa"})
+        testVal := "Hello Kappa!"
+        if returnVal != testVal {
+		t.Errorf("Return incorrect, got: %s, expected: %s",returnVal,testVal)
+	}
+	print (a);
+}
+    
