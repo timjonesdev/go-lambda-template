105c5dd8aca52ccd56fc9003f30f797c67e77f68
Ryan Monroe <rx364p@att.com>
2019-09-19 16:41:19 -0700

    adding default unit test

--- base/main_test.go
+++ base/main_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestHandleRequest(t *testing.T) {
+	returnVal,a := HandleRequest(nil, MyEvent{"Kappa"})
+        testVal := "Hello Kappa!"
+        if returnVal != testVal {
+		t.Errorf("Return incorrect, got: %s, expected: %s",returnVal,testVal)
+	}
+	_ = a
+}
